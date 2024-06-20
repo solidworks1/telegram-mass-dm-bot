3ac97dcf58de4147105cd5ac7944fac093ea666f
Solid Works <cesurenes35++@gmail.com>
2024-06-20 17:41:52 +0300

    Add files via upload

---
+++ telegram-mass-dmbot.py
@@ -0,0 +1,9 @@
+#This file has no features, please review the README.MD file.
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
